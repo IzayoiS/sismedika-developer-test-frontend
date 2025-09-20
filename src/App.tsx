f253a21fd6af6be6cc886ea0122ebc95c39c7b3a
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-20 08:53:12 +0700

    feat: setup app routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LoginPage from "./features/auth/components/loginPage";
 import RegisterPage from "./features/auth/components/registerPage";
+import Foods from "./features/foods/components/foodPage";
+import Tables from "./features/tables/components/tablesPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />{" "}
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/foods" element={<Foods />} />
+        <Route path="/tables" element={<Tables />} />
       </Routes>
     </Router>
   );
