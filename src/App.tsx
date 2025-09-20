77f8f93fdca131437f252041d33502ac6858ffae
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-20 21:57:17 +0700

    refactor: migrate to RouterProvider for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import LoginPage from "./features/auth/components/loginPage";
-import RegisterPage from "./features/auth/components/registerPage";
-import Foods from "./features/foods/components/foodPage";
-import Tables from "./features/tables/components/tablesPage";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/foods" element={<Foods />} />
-        <Route path="/tables" element={<Tables />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
