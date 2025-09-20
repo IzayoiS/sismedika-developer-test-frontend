711c1fc889fe90c6e241f983eabec70f94411d66
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-20 08:52:56 +0700

    feat: setup app providers and query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/toaster.tsx";
+import { Provider } from "./components/ui/provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+        <Toaster />
+      </Provider>
+    </QueryClientProvider>
+  </StrictMode>
+);
